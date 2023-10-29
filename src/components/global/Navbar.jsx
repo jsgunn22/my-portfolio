35e4872d4dad5b855ce85385210106a0913f6509
Jeffrey Gunn <j.s.gunn22@gmail.com>
2023-10-28 20:06:29 -0700

    playing with bubbles

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -1,5 +1,40 @@
 import Button from "../atoms/button";
 import proficiencies from "../../../proficiencyLogos";
+import React, { useState } from "react";
+
+function ProfBubble(props) {
+  const [ishovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div className="w-10 -ml-3">
+      <img
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        key={props.key}
+        src={props.src}
+        alt={props.alt}
+        className={`h-10 rounded-full relative -ml-3 shadow-md hover:shadow-lg hover:scale-125 cursor-pointer`}
+        style={{ zIndex: ishovered ? 1000 : 60 - props.key * 10 }}
+      />
+      <div
+        className={`absolute  -ml-6 bg-neu-0 p-1 text-sm rounded-sm pt-6 top-10 ${
+          ishovered ? "block" : "hidden"
+        }`}
+        // style={{ zIndex: 99 }}
+      >
+        <p>This is a test</p>
+      </div>
+    </div>
+  );
+}
 
 function ProficiencyBubbles() {
   const profLogos = [];
@@ -7,17 +42,10 @@ function ProficiencyBubbles() {
     profLogos.push(proficiencies[i]);
   }
 
-  console.log(profLogos);
   return (
-    <div className="h-full w-full flex py-4 px-6 ">
+    <div className="h-full w-full flex py-4 pl-12 px-6 ">
       {profLogos.map((logo, index) => (
-        <img
-          key={index}
-          src={logo.src}
-          alt={logo.logo}
-          className={`h-10 rounded-full relative -ml-3 shadow-md hover:shadow-lg hover:scale-125 cursor-pointer`}
-          style={{ zIndex: 60 - index * 10 }}
-        />
+        <ProfBubble key={index} src={logo.src} alt={logo.logo} />
       ))}
       <div
         className="rounded-full bg-neu-0 h-10 w-10 relative -ml-3 shadow-md hover:shadow-lg hover:scale-125 cursor-pointer"
